6394e5937857d1db88a487f6adcc6743304cb4fb
Anton <popsof@yandex.ru>
2024-08-06 16:05:22 +0300

    Изменения в app.js и index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from './style.module.css';
+import MyInput from './components/UI/form/MyInput';
+import { useState } from 'react';
+import ProgressBar from './components/UI/progress/ProgressBar';
+
 
 function App() {
+
+  const [maxQuests,setMaxQuests] = useState();
+  const [currentQuets,setCurrentQuests] = useState()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.App}>
+      <ProgressBar maxQuests={maxQuests} currentQuets={currentQuets}></ProgressBar>
+      <MyInput setMaxQuests={setMaxQuests} setCurrentQuests={setCurrentQuests} />
     </div>
   );
 }
